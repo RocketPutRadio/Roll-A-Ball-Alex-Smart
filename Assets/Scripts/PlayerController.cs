b2eb50420ddb6ef4248445d9b8830865e64ddd80
RocketPutRadio <alexander.smart@discoverystudent.org>
2025-11-03 10:32:31 -0500

    file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,27 @@ using UnityEngine. InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
-    Void OnMove(InputValue movementValue){
-        
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>(); 
+    }
+
+    { 
+        private void FixedUpdate(){
+        rb.AddForce (movementVector);
+    }
+}
 }

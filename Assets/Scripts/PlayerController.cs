2c9e0b07bcdb41e0efa011240b670fb0770c60f8
RocketPutRadio <alexander.smart@discoverystudent.org>
2025-11-04 11:30:50 -0500

    woah

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     
+    private float movementX;
+    private float movementY;
+
+    public float speed = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,16 +18,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        
     }
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>(); 
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
     }
 
-    { 
+    
         private void FixedUpdate(){
-        rb.AddForce (movementVector);
+                Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce (movement);
     }
-}
+
 }
